cf7518ef71fb6f35de85ce469f67fbe2830615ca
hank9898 <970064680@qq.com>
2021-04-05 21:45:26 +0800

    完成了登录功能

--- src/main.js
+++ src/main.js
@@ -2,10 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+// 导入字体图标
+import './assets/fonts/iconfont.css'
+// 导入全局样式表
+import './assets/css/global.css'
+
+// 挂载axios到Vue的原型对象上，每个vue组件通过this可以访问http，发起Ajax请求
+import axios from 'axios'
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+        router,
+        // 渲染App.vue根组件到页面上
+        render: h => h(App)
+    }).$mount('#app') // 挂载到.app标签里，作用等同“el:"#app"”
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,11 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import { Button, Form, FormItem, Input, Message } from 'element-ui'
 
+// 注册为全局可用组件
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+
+// 全局挂载，message自定义属性
+Vue.prototype.$message = Message
