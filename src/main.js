fa77a29b3c2aea86bcbc826fb949ef148d189e82
hank9898 <970064680@qq.com>
2021-04-11 16:57:29 +0800

    完成商品功能开发

--- src/main.js
+++ src/main.js
@@ -8,6 +8,12 @@ import './assets/fonts/iconfont.css'
 import './assets/css/global.css'
 // 导入第三方依赖/插件
 import TreeTable from 'vue-table-with-tree-grid'
+// 导入第三方依赖，关于markdown
+import VueQuillEditor from 'vue-quill-editor'
+// 导入markdown编辑器的相关样式
+import 'quill/dist/quill.core.css' // import styles
+import 'quill/dist/quill.snow.css' // for snow theme
+import 'quill/dist/quill.bubble.css' // for bubble theme
 
 // 挂载axios到Vue的原型对象上，每个vue组件通过this可以访问http，发起Ajax请求
 import axios from 'axios'
@@ -20,8 +26,26 @@ axios.interceptors.request.use(config => {
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
-    // 手动注册，全局可用
+
+// 手动注册，全局可用
 Vue.component('tree-table', TreeTable)
+Vue.use(VueQuillEditor)
+
+// 全局时间过滤器
+// 第一个参数 过滤器名称
+Vue.filter('dateFormat', (originVal) => {
+    const dt = new Date(originVal)
+
+    const y = dt.getFullYear()
+    const m = (dt.getMonth() + 1 + '').padStart(2, '0') // padStart()函数，位数补充，从头开始，0为补位数值
+    const d = (dt.getSeconds() + '').padStart(2, '0')
+
+    const hh = (dt.getHours() + '').padStart(2, '0')
+    const mm = (dt.getMinutes() + '').padStart(2, '0')
+    const ss = (dt.getSeconds() + '').padStart(2, '0')
+
+    return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
 
 new Vue({
         router,
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -31,7 +31,12 @@ import {
     Cascader,
     Alert,
     Tabs,
-    TabPane
+    TabPane,
+    Steps,
+    Step,
+    CheckboxGroup,
+    Checkbox,
+    Upload
 } from 'element-ui'
 
 // 注册为全局可用组件
@@ -65,6 +70,11 @@ Vue.use(Cascader)
 Vue.use(Alert)
 Vue.use(Tabs)
 Vue.use(TabPane)
+Vue.use(Steps)
+Vue.use(Step)
+Vue.use(CheckboxGroup)
+Vue.use(Checkbox)
+Vue.use(Upload)
 
 // 全局挂载，message自定义属性
 Vue.prototype.$message = Message
